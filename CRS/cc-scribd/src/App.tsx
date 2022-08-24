91e2005901da9b85a66f4e3223146fbb32ac4001
ccpau <chloe@pau.be>
2022-08-24 15:36:49 +0200

    CRS-3/create-website

---
+++ CRS/cc-scribd/src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>CC SCRIBD</h1>
+        <h2>LINKING: GIT</h2>
+        <h3>LINKING: Netlify </h3>
+        <h4> DEV | PRD </h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
